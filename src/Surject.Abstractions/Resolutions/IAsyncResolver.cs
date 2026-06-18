e94bac122953035f5ba17b0d375a74bdfa3afed3
7Swaize <rahejasachit@gmail.com>
2026-06-18 02:37:25 -0400

    Update Resolver interfaces for complete combinatorial expansion

--- src/Surject.Abstractions/Resolutions/IAsyncResolver.cs
+++ src/Surject.Abstractions/Resolutions/IAsyncResolver.cs
@@ -4,8 +4,16 @@ using System.Threading.Tasks;
 namespace Surject.Abstractions.Resolutions;
 
 public interface IAsyncResolver {
-    public ValueTask<T> ResolveAsync<T>(CancellationToken c = default) where T : class;
-    public ValueTask<T?> ResolveAsyncOptional<T>(CancellationToken c = default) where T : class;
-    public ValueTask<T> ResolveAsyncPrimary<T>(CancellationToken c = default) where T : class;
-    public ValueTask<T[]> ResolveAsyncAll<T>(CancellationToken c = default) where T : class;
+    ValueTask<T> ResolveAsync<T>(CancellationToken ct = default) where T : class;
+    ValueTask<T?> ResolveOptionalAsync<T>(CancellationToken ct = default) where T : class;
+    ValueTask<T> ResolvePrimaryAsync<T>(CancellationToken ct = default) where T : class;
+    ValueTask<T?> ResolveOptionalPrimaryAsync<T>(CancellationToken ct = default) where T : class;
+
+    ValueTask<T> ResolveKeyedAsync<T>(string key, CancellationToken ct = default) where T : class;
+    ValueTask<T?> ResolveOptionalKeyedAsync<T>(string key, CancellationToken ct = default) where T : class;
+    ValueTask<T> ResolvePrimaryKeyedAsync<T>(string key, CancellationToken ct = default) where T : class;
+    ValueTask<T?> ResolveOptionalPrimaryKeyedAsync<T>(string key, CancellationToken ct = default) where T : class;
+
+    ValueTask<T[]> ResolveAllAsync<T>(CancellationToken ct = default) where T : class;
+    ValueTask<T[]> ResolveAllKeyedAsync<T>(string key, CancellationToken ct = default) where T : class;
 }
--- src/Surject.Abstractions/Resolutions/IResolver.cs
+++ src/Surject.Abstractions/Resolutions/IResolver.cs
@@ -3,15 +3,26 @@ using System;
 namespace Surject.Abstractions.Resolutions;
 
 public interface IResolver : IAsyncResolver {
-    public T Resolve<T>() where T : class;
-    public T? ResolveOptional<T>() where T : class;
-    public T ResolveKeyed<T>(string key) where T : class;
-    public T? ResolveKeyedOptional<T>(string key) where T : class;
-    public Lazy<T> ResolveLazy<T>() where T : class;
+    T Resolve<T>() where T : class;
+    T? ResolveOptional<T>() where T : class;
+    T ResolvePrimary<T>() where T : class;
+    T? ResolveOptionalPrimary<T>() where T : class;
+
+    T ResolveKeyed<T>(string key) where T : class;
+    T? ResolveOptionalKeyed<T>(string key) where T : class;
+    T ResolvePrimaryKeyed<T>(string key) where T : class;
+    T? ResolveOptionalPrimaryKeyed<T>(string key) where T : class;
     
-    public T ResolvePrimary<T>() where T : class;
-    public T? ResolvePrimaryOptional<T>() where T : class;
-    
-    public T[] ResolveAll<T>() where T : class;
-    public T[] ResolveAllKeyed<T>(string key) where T : class;
+    Lazy<T> ResolveLazy<T>() where T : class;
+    Lazy<T?> ResolveOptionalLazy<T>() where T : class;
+    Lazy<T> ResolvePrimaryLazy<T>() where T : class;
+    Lazy<T?> ResolveOptionalPrimaryLazy<T>() where T : class;
+
+    Lazy<T> ResolveKeyedLazy<T>(string key) where T : class;
+    Lazy<T?> ResolveOptionalKeyedLazy<T>(string key) where T : class;
+    Lazy<T> ResolvePrimaryKeyedLazy<T>(string key) where T : class;
+    Lazy<T?> ResolveOptionalPrimaryKeyedLazy<T>(string key) where T : class;
+
+    T[] ResolveAll<T>() where T : class;
+    T[] ResolveAllKeyed<T>(string key) where T : class;
 }
