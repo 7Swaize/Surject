07e16d5673efb02bd8925ba66016b01d9344c44b
7Swaize <rahejasachit@gmail.com>
2026-06-19 19:30:20 -0400

    Add SurjectExtensions

---
+++ src/Surject.Unity/SurjectExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Surject.Unity;
+
+public static class SurjectExtensions {
+    public static List<TTarget> PreformTraversalWithBoundary<TTarget, TBoundary>(GameObject self, Func<GameObject, TTarget[]> selector) 
+        where TTarget : class
+        where TBoundary : Component
+    {
+        List<TTarget> result = [];
+        TTarget[] candidates = selector(self);
+        
+        foreach (TTarget candidate in candidates) {
+            Component? component = candidate as Component;
+
+            if (component == null) {
+                continue;
+            }
+
+            if (ReferenceEquals(component.GetComponentInParent<TBoundary>().gameObject, self)) {
+                result.Add(candidate);
+            }
+        }
+        
+        return result;
+    }
+}
