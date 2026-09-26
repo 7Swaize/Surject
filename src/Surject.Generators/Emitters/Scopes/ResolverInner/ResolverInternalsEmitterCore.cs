b1c2a2d53de485fdbb555461c02ed2953caa705a
7Swaize <rahejasachit@gmail.com>
2026-09-26 15:04:13 -0400

    Emit resolver fields and ctor

--- src/Surject.Generators/Emitters/Scopes/ResolverInner/ResolverInternalsEmitterCore.cs
+++ src/Surject.Generators/Emitters/Scopes/ResolverInner/ResolverInternalsEmitterCore.cs
@@ -1,5 +1,8 @@
 using System.CodeDom.Compiler;
+using Surject.Abstractions.Resolutions;
+using Surject.Generators.Emitters.Helpers;
 using Surject.Generators.Models.Concepts;
+using Surject.Generators.Models.Primitives;
 
 namespace Surject.Generators.Emitters.Scopes.ResolverInner;
 
@@ -12,5 +15,35 @@ internal readonly ref struct ResolverInternalsEmitterCore : IChainedEmitter {
         _linkage = linkage;
     }
 
-    public void Emit(IndentedTextWriter writer) { }
+    public void Emit(IndentedTextWriter writer) {
+        EmitMembers(writer);
+        EmitCtor(writer);
+    }
+
+    private void EmitMembers(IndentedTextWriter writer) {
+        ITypeReferenceModel containerType = _container.Decl.AsTypeRef;
+        
+        writer.WriteLine($"private readonly {BuildHelpers.BuildContainerType(containerType)} _c;");
+        writer.WriteLine($"private readonly global::{typeof(IResolver).FullName}? _parent;");
+        writer.WriteLine($"private readonly {containerType.FQNConstructedArgBased} _scopeProvider;");
+    }
+
+    private void EmitCtor(IndentedTextWriter writer) {
+        ITypeReferenceModel containerType = _container.Decl.AsTypeRef;
+        
+        writer.WriteMultiline(
+            $$"""
+              internal {{BuildHelpers.BuildResolverType(containerType)}}(
+                    {{BuildHelpers.BuildContainerType(containerType)}} c,
+                    global::{typeof(IResolver).FullName}? parent,
+                    {{containerType.FQNConstructedArgBased}} scopeProvider)
+                {
+                    _c = c;
+                    _parent = parent;
+                    _scopeProvider = scopeProvider;
+                }
+                
+              """
+        );
+    }
 }
