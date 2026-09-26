90b51f0c9f5205267af76e9bac93419acf4a5ec8
7Swaize <rahejasachit@gmail.com>
2026-09-26 13:44:36 -0400

    Bump Bcl.AsyncInterfaces pacakge version

--- tests/Surject.Tests/Utility.cs
+++ tests/Surject.Tests/Utility.cs
@@ -8,11 +8,14 @@ using UnityEngine;
 namespace Surject.Tests;
 
 public static class Utility {
+	public static ReferenceAssemblies NetStandard20ReferenceAssemblies => ReferenceAssemblies.NetStandard.NetStandard20;
+	public static ImmutableArray<PortableExecutableReference> NetStandard20Assemblies => Basic.Reference.Assemblies.NetStandard20.References.All;
+	
 #if NET8_0
 	public static ReferenceAssemblies ReferenceAssemblies => Microsoft.CodeAnalysis.Testing.ReferenceAssemblies.Net.Net80;
 	public static IEnumerable<MetadataReference> NetCoreAssemblies => Basic.Reference.Assemblies.Net80.References.All;
 #elif NET9_0
-	public static ReferenceAssemblies ReferenceAssemblies => ReferenceAssemblies.Net.Net90;
+	public static ReferenceAssemblies ReferenceAssemblies => Microsoft.CodeAnalysis.Testing.ReferenceAssemblies.Net.Net90;
 	public static IEnumerable<MetadataReference> NetCoreAssemblies => Basic.Reference.Assemblies.Net90.References.All;
 #elif NET10_0
     public static ReferenceAssemblies ReferenceAssemblies => Microsoft.CodeAnalysis.Testing.ReferenceAssemblies.Net.Net100;
@@ -20,8 +23,8 @@ public static class Utility {
 #endif
 
     public static MetadataReference[] GetAdditionalReferences() => [
-        MetadataReference.CreateFromFile(typeof(InjectAttribute).Assembly.Location),
-        MetadataReference.CreateFromFile(typeof(SurjectRuntime).Assembly.Location),
-        MetadataReference.CreateFromFile(typeof(MonoBehaviour).Assembly.Location)
+        MetadataReference.CreateFromFile(typeof(InjectAttribute).Assembly.Location), // Surject.Abstractions
+        MetadataReference.CreateFromFile(typeof(SurjectRuntime).Assembly.Location), // Surject.Unity
+        MetadataReference.CreateFromFile(typeof(MonoBehaviour).Assembly.Location) // UnityEngine (shims)
     ];
 }
